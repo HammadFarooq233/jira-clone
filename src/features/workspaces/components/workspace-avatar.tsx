69634cc4ddfed8fca0b9a99d3bf8ca6cc5faf9bf
Hammad Farooq <hammadfarooq233@gmail.com>
2024-11-13 14:50:54 +0500

    create UI for displaying workspaces in the sidebar

---
+++ src/features/workspaces/components/workspace-avatar.tsx
@@ -0,0 +1,33 @@
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+
+interface WorkspaceAvatarProps {
+  name: string;
+  image?: string;
+  className?: string;
+}
+
+export default function WorkspaceAvatar({
+  name,
+  className,
+  image,
+}: WorkspaceAvatarProps) {
+  if (image) {
+    return (
+      <div
+        className={cn("relative size-10 overflow-hidden rounded-md", className)}
+      >
+        <Image src={image} alt={name} fill className="object-cover" />
+      </div>
+    );
+  }
+
+  return (
+    <Avatar className={cn("size-10 rounded-md", className)}>
+      <AvatarFallback className="bg-blue-600 text-lg font-semibold uppercase text-white rounded-md">
+        {name[0]}
+      </AvatarFallback>
+    </Avatar>
+  );
+}
