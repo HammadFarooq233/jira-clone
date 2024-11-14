166bbc2bc4089fb870c44b89f8e6717891dcdcc5
Hammad Farooq <hammadfarooq233@gmail.com>
2024-11-14 15:14:04 +0500

    handle redirection based on available workspaces

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,15 +1,32 @@
 import { getCurrent } from "@/features/auth/actions";
-import CreateWorkspaceForm from "@/features/workspaces/components/create-workspace-form";
+import { AUTH_COOKIE } from "@/features/auth/constants";
+import { client } from "@/lib/rpc";
+import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 export default async function Page() {
   const user = await getCurrent();
-
   if (!user) redirect("/sign-in");
 
-  return (
-    <div className="h-full bg-neutral-500 p-4">
-      <CreateWorkspaceForm />
-    </div>
+  const cookieStore = cookies();
+  const session = cookieStore.get(AUTH_COOKIE);
+
+  const response = await client.api.workspaces.$get(
+    {},
+    {
+      headers: {
+        Cookie: `${AUTH_COOKIE}=${session?.value}`,
+      },
+    },
   );
+
+  const { data: workspaces } = await response.json();
+
+  if (workspaces.documents.length === 0) {
+    redirect("/workspaces/create");
+  } else {
+    redirect(`/workspaces/${workspaces.documents[0].$id}`);
+  }
+
+  return <div></div>;
 }
