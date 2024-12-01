f1c5091a1b554e608ba018239021da112dbdf009
Hammad Farooq <hammadfarooq233@gmail.com>
2024-12-01 23:19:14 +0500

    feat: add UI for switching between views of tasks

---
+++ src/features/tasks/components/task-view.switcher.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { DottedSeparator } from "@/components/dotted-separator";
+import { Button } from "@/components/ui/button";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { PlusIcon } from "lucide-react";
+import { useCreateTaskModal } from "../hooks/use-create-task-modal";
+
+export default function TaskViewSwitcher() {
+  const { open } = useCreateTaskModal();
+
+  return (
+    <Tabs className="w-full flex-1 rounded-lg border">
+      <div className="flex h-full flex-col overflow-auto p-4">
+        <div className="flex flex-col items-center justify-between gap-y-2 lg:flex-row">
+          <TabsList className="w-full lg:w-auto">
+            <TabsTrigger className="w-full lg:w-auto" value="table">
+              Table
+            </TabsTrigger>
+
+            <TabsTrigger className="w-full lg:w-auto" value="kanban">
+              Kanban
+            </TabsTrigger>
+
+            <TabsTrigger className="w-full lg:w-auto" value="calendar">
+              Calendar
+            </TabsTrigger>
+          </TabsList>
+
+          <Button size={"sm"} className="w-full lg:w-auto" onClick={open}>
+            <PlusIcon className="mr-2 size-4" />
+            New
+          </Button>
+        </div>
+        <DottedSeparator className="my-4" />
+        Data filters
+        <DottedSeparator className="my-4" />
+        <>
+          <TabsContent value="table" className="mt-0">
+            Data table
+          </TabsContent>
+
+          <TabsContent value="kanban" className="mt-0">
+            Data kanban
+          </TabsContent>
+
+          <TabsContent value="calendar" className="mt-0">
+            Data calendar
+          </TabsContent>
+        </>
+      </div>
+    </Tabs>
+  );
+}
