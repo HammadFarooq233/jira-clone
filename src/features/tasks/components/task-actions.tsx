085e9b0d87993e7401f7006d4cdce325238325a0
Hammad Farooq <hammadfarooq233@gmail.com>
2024-12-06 14:29:29 +0500

    feat: implement task deletion

--- src/features/tasks/components/task-actions.tsx
+++ src/features/tasks/components/task-actions.tsx
@@ -4,7 +4,9 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { useConfirm } from "@/hooks/use-confirm";
 import { ExternalLinkIcon, PencilIcon, TrashIcon } from "lucide-react";
+import { useDeleteTask } from "../api/use-delete-task";
 
 interface TaskActionsProps {
   id: string;
@@ -14,61 +16,57 @@ interface TaskActionsProps {
 
 export default function TaskActions({
   id,
-  projectId,
+  // projectId,
   children,
 }: TaskActionsProps) {
+  const [ConfirmDialog, confirm] = useConfirm(
+    "Delete Task",
+    "This action cannot be undone",
+    "destructive",
+  );
+
+  const { mutate, isPending } = useDeleteTask();
+
+  const onDelete = async () => {
+    const ok = await confirm();
+    if (!ok) return;
+
+    mutate({ param: { taskId: id } });
+  };
+
   return (
     <div className="flex justify-end">
+      <ConfirmDialog />
+
       <DropdownMenu modal={false}>
         <DropdownMenuTrigger asChild>{children}</DropdownMenuTrigger>
+
+        <DropdownMenuContent align="end" className="w-48">
+          <DropdownMenuItem className="p-[10px] font-medium" onClick={() => {}}>
+            <ExternalLinkIcon className="mr-2 size-4 stroke-2" />
+            Task Details
+          </DropdownMenuItem>
+
+          <DropdownMenuItem className="p-[10px] font-medium" onClick={() => {}}>
+            <ExternalLinkIcon className="mr-2 size-4 stroke-2" />
+            Open Project
+          </DropdownMenuItem>
+
+          <DropdownMenuItem className="p-[10px] font-medium" onClick={() => {}}>
+            <PencilIcon className="mr-2 size-4 stroke-2" />
+            Edit Task
+          </DropdownMenuItem>
+
+          <DropdownMenuItem
+            className="p-[10px] font-medium text-amber-700 focus:text-amber-700"
+            onClick={onDelete}
+            disabled={isPending}
+          >
+            <TrashIcon className="mr-2 size-4 stroke-2" />
+            Delete Task
+          </DropdownMenuItem>
+        </DropdownMenuContent>
       </DropdownMenu>
-
-      <DropdownMenuContent align="end" className="w-48">
-        <DropdownMenuItem
-          disabled={false}
-          className="p-[10px] font-medium"
-          onClick={() => {}}
-        >
-          <ExternalLinkIcon className="mr-2 size-4 stroke-2" />
-          Task Details
-        </DropdownMenuItem>
-
-        <DropdownMenuItem
-          disabled={false}
-          className="p-[10px] font-medium"
-          onClick={() => {}}
-        >
-          <ExternalLinkIcon className="mr-2 size-4 stroke-2" />
-          Open Project
-        </DropdownMenuItem>
-
-        <DropdownMenuItem
-          disabled={false}
-          className="p-[10px] font-medium"
-          onClick={() => {}}
-        >
-          <PencilIcon className="mr-2 size-4 stroke-2" />
-          Edit Task
-        </DropdownMenuItem>
-
-        <DropdownMenuItem
-          disabled={false}
-          className="p-[10px] font-medium"
-          onClick={() => {}}
-        >
-          <ExternalLinkIcon className="mr-2 size-4 stroke-2" />
-          Open Project
-        </DropdownMenuItem>
-
-        <DropdownMenuItem
-          disabled={false}
-          className="p-[10px] font-medium text-amber-700 focus:text-amber-700"
-          onClick={() => {}}
-        >
-          <TrashIcon className="mr-2 size-4 stroke-2" />
-          Delete Task
-        </DropdownMenuItem>
-      </DropdownMenuContent>
     </div>
   );
 }
