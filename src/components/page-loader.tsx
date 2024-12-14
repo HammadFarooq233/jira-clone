e3591ba96add70b1a618cce8b938c2b09df905c2
Hammad Farooq <hammadfarooq233@gmail.com>
2024-12-14 22:58:25 +0500

    refactor: fix type in component name

--- src/components/page-loader.tsx
+++ src/components/page-loader.tsx
@@ -2,7 +2,7 @@
 
 import { Loader } from "lucide-react";
 
-export default function LoadingPage() {
+export default function PageLoader() {
   return (
     <div className="flex h-screen flex-col items-center justify-center">
       <Loader className="size-6 animate-spin text-muted-foreground" />
