5efaac021796b49e1e127ee89f720ff60290a553
Hammad Farooq <hammadfarooq233@gmail.com>
2024-12-14 22:56:04 +0500

    refactor: remove unused files

--- src/app/(dashboard)/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { Loader } from "lucide-react";
-
-export default function Loading() {
-  return (
-    <div className="flex min-h-screen items-center justify-center">
-      <Loader className="size-6 animate-spin text-muted-foreground" />
-    </div>
-  );
-}
